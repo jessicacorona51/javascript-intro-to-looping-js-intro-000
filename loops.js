6995634fe9775f23c23616420efa5116aac73a7a
Jessica Corona <jessicacorona51@yahoo.com>
2020-06-11 21:50:35 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,4 +1,5 @@
-function forLoop (array)
-for (let i = 0; i < 25; i++) {
-  console.log(`I am ${[i]} strange loop$!`)
+function forLoop (array) {
+  for (let i = 0; i < 25; i++) {
+    console.log(`I am ${[i]} strange loop$!`)
+  }
 }
