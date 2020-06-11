9def21d7e585acd4a065d2a6be5f59af7a9ca658
Jessica Corona <jessicacorona51@yahoo.com>
2020-06-11 23:08:45 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,3 +16,13 @@ function whileLoop(n) {
     }
     return "done";
 }
+
+function doWhileLoop(num) {
+  let i = 0;
+  do {
+    console.log(i, "I run once regardless. ");
+  }
+  while (++i < num);
+  console.log("Final value of i:", i);
+}
+doWhileLoop(10);
