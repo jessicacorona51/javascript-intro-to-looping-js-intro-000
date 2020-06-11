d00ea0ea7b3b20d6663970993f7b70a7b83cb482
Jessica Corona <jessicacorona51@yahoo.com>
2020-06-11 22:33:45 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,3 +16,13 @@ function whileLoop(n) {
     }
     return "done";
 }
+function doWhileLoop(num) {
+  function maybeTrue() {
+   return Math.random() >= 0.5
+ }
+
+ do {
+   num.pop();
+ } while (num.length > -1 && maybeTrue());
+   return num;
+}
