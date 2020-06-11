309d48163b4794191cb7899f955faa4220d63e12
Jessica Corona <jessicacorona51@yahoo.com>
2020-06-11 22:43:57 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -27,14 +27,3 @@ function doWhileLoop(num)  {
    }
   while(incrementVariable() <= num);
  }
- function doWhileLoop(num)  {
-    var i = 0;
-   function incrementVariable() {
-     i++
-     return i;
-   }
-   do {
-     console.log("I run once regardless.");
-    }
-   while(incrementVariable() =< 10);
-  }
